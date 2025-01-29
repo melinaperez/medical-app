c59126323cb8a61b07ca06b8c4b9cc67e7382651
Melina Perez <perez.melina@outlook.es>
2025-01-28 23:29:36 -0300

    Cuarto commit

--- lib/roles.ts
+++ lib/roles.ts
@@ -1,4 +1,4 @@
-export const ADMIN_EMAILS = ["meli.y.perez92@gmail.com"]
+export const ADMIN_EMAILS = ["meli.y.perez92@gmail.com", "daniel.l.piskorz@gmail.com", "gonzalopiskorz@gmail.com"]
 
 export function isAdminEmail(email: string | null | undefined) {
   return email ? ADMIN_EMAILS.includes(email) : false
